91be6effdb0fab15fb167d1378ef8c6a771a772b
Alexander1995-it <sany_nax@mail.ru>
2022-11-12 20:30:47 +0300

    reg 1

--- src/pages/registrationPage/RegistrationPage.tsx
+++ src/pages/registrationPage/RegistrationPage.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export  const RegistrationPage = () => {
     return (
         <div>
-            Registration PAGE
+            Registration PAGE111111
         </div>
     );
 };
