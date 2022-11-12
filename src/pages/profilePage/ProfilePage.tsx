307ba068d8d99e5cf75f6fa86b42b38514714fd1
Maxim Dmitriev <Renissance@tut.by>
2022-11-12 22:14:41 +0300

    Profile Page Css+HTML

--- src/pages/profilePage/ProfilePage.tsx
+++ src/pages/profilePage/ProfilePage.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import styleProfile from './ProfilePage.module.css'
 
 export const ProfilePage = () => {
     return (
         <div>
-            Profile Page
+            <div><a href="/" className={styleProfile.back_link}> Back to Packs List</a></div>
+            <div>
+                <h2 className={styleProfile.title}>Personal Information</h2>
+
+                <img src="" alt=""/>
+                <div>Name</div>
+                <div>email</div>
+
+                <button>Log out</button>
+
+
+            </div>
         </div>
     );
 };
