dc75a4072429ce403d7562b3b6a2ddae5fa9a7e7
Maxim Dmitriev <Renissance@tut.by>
2022-11-12 22:25:49 +0300

    Profile Page Css+HTML 2

--- src/pages/profilePage/ProfilePage.tsx
+++ src/pages/profilePage/ProfilePage.tsx
@@ -3,9 +3,9 @@ import styleProfile from './ProfilePage.module.css'
 
 export const ProfilePage = () => {
     return (
-        <div>
+        <div className={styleProfile.ProfilePage}>
             <div><a href="/" className={styleProfile.back_link}> Back to Packs List</a></div>
-            <div>
+            <div className={styleProfile.wrapper}>
                 <h2 className={styleProfile.title}>Personal Information</h2>
 
                 <img src="" alt=""/>
