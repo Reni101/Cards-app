baff6ca1aa6991cee38c00f12b107baf905e1c4b
Maxim Dmitriev <Renissance@tut.by>
2022-11-13 11:13:14 +0300

    Profile Page Logged In

--- src/pages/profilePage/ProfilePage.tsx
+++ src/pages/profilePage/ProfilePage.tsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import styleProfile from './ProfilePage.module.css'
+import {useAppDispatch} from "../../hooks/hooks";
+import {Navigate} from "react-router-dom";
 
 export const ProfilePage = () => {
+    const dispatch = useAppDispatch()
+    useEffect(() => {
+        if (false) { //!loggedIn
+            //;
+        }
+        //dispatch(setProfile)
+    }, [])
+
+    if (false) { //!loggedIn
+        return <Navigate to='/login'/>
+    }
     return (
         <div className={styleProfile.ProfilePage}>
             <div><a href="/" className={styleProfile.back_link}> Back to Packs List</a></div>
---
+++ src/pages/profilePage/profilePageReducer/ProfilePagerRedicer.ts
@@ -0,0 +1,34 @@
+import {AppThunk} from "../../../Redux/Store";
+
+
+const initialState = {}
+
+type InitialStateType = typeof initialState
+
+export const ProfilePage = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+    switch (action.type) {
+        case ' ':
+            return state
+        default:
+            return state
+    }
+}
+//=============================AC======================================
+export const AC = () => ({
+    type: ' ',
+
+} as const)
+
+
+//==============================TC============================
+
+export const TC = (): AppThunk => async dispatch => {
+
+}
+
+
+//
+
+export type ActionsType =
+    | ReturnType<typeof AC>
+
