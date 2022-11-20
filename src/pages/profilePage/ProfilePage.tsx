345a440719611e2203fae11625d7846e509cbf91
Maxim Dmitriev <Renissance@tut.by>
2022-11-20 17:28:52 +0300

    Merge branch success + Pagination

--- src/pages/profilePage/ProfilePage.tsx
+++ src/pages/profilePage/ProfilePage.tsx
@@ -10,6 +10,7 @@ import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import {requestStatusType} from "../../AppReducer";
 
 import {SingOutTC} from "../login/loginReducer/LoginReducer";
+import {Paginator} from "../../common/Paginator/Paginator";
 
 export const ProfilePage = () => {
 
@@ -53,6 +54,7 @@ export const ProfilePage = () => {
                 </Button>
 
             </div>
+            <Paginator cardPacksTotalCount={199} packsCount={10} currentPage={1} />
         </Slide>
     );
 };
