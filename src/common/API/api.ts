d172a655e019fd150d57fb0ebddd6f065e86536b
kotpavlik <kotpavlik88@gmail.com>
2022-11-24 15:32:26 +0300

    feat: table UI Packs and Cards logic

--- src/common/API/api.ts
+++ src/common/API/api.ts
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-
+//process.env.NODE_ENV === 'development' ? 'http://localhost:7542/2.0/' :
 export const instance = axios.create({
-    baseURL: process.env.NODE_ENV === 'development' ? 'http://localhost:7542/2.0/' : process.env.REACT_APP_BACK_URL,
+    baseURL:  process.env.REACT_APP_BACK_URL,
     withCredentials: true,
 })
 
