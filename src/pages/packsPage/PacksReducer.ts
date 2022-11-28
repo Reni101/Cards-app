03ce75ba44b1ccc4b42f4c034eaa9039743b0269
kotpavlik <kotpavlik88@gmail.com>
2022-11-28 03:42:10 +0300

    fix: two request this is bool shit

--- src/pages/packsPage/PacksReducer.ts
+++ src/pages/packsPage/PacksReducer.ts
@@ -1,8 +1,9 @@
-import {AppThunk} from "../../Redux/Store";
-import {setStatusApp} from "../../AppReducer";
-import {AxiosError} from "axios";
-import {packsAPI, RequestAddPackType, RequestUpdatePackType, ResponseCardsType} from "./PacksAPI";
-import {handleError} from "../../common/ErrorUtils/errorFunck";
+import {AppThunk} from '../../Redux/Store';
+import {setStatusApp} from '../../AppReducer';
+import {AxiosError} from 'axios';
+import {packsAPI, RequestAddPackType, RequestUpdatePackType, ResponseCardsType} from './PacksAPI';
+import {handleError} from '../../common/ErrorUtils/errorFunck';
+import {setCardsAC, setCardsTC, setPackNameForCardAC, setPacksIdAC} from '../cardsPage/CardsReducer';
 
 export type ActionsPacksType =
     | ReturnType<typeof setPacksAC>
@@ -55,9 +56,9 @@ const initialState: InitialStateType = {
     pageCount: 5,
     min: 0,
     max: MAX_COUNT_CARDS,
-    sortPacks: "",
-    packName: "",
-    user_id: "",
+    sortPacks: '',
+    packName: null,
+    user_id: '',
 }
 
 
@@ -65,20 +66,20 @@ export const PacksReducer = (state: InitialStateType = initialState, action: Act
     switch (action.type) {
         case 'PACKS/SET_PACKS':
             return {...state, ...action.payload}
-        case "PACKS/CHANGE_PAGE":
+        case 'PACKS/CHANGE_PAGE':
             return {...state, page: action.payload.page}
-        case "PACKS/CHANGE_MIN": {
+        case 'PACKS/CHANGE_MIN': {
             return {...state, min: action.payload.min}
         }
-        case "PACKS/CHANGE_MAX":
+        case 'PACKS/CHANGE_MAX':
             return {...state, max: action.payload.max}
-        case "PACKS/CHANGE_PAGE_COUNT":
+        case 'PACKS/CHANGE_PAGE_COUNT':
             return {...state, pageCount: action.payload.pageCount}
-        case "PACKS/CHANGE_SORT_PACK":
+        case 'PACKS/CHANGE_SORT_PACK':
             return {...state, sortPacks: action.payload.sortPacks}
-        case "PACKS/SORT_PACKS_NAME":
+        case 'PACKS/SORT_PACKS_NAME':
             return {...state, packName: action.payload.packName}
-        case "PACKS/CHANGE_SHOW_MY_PACKS":
+        case 'PACKS/CHANGE_SHOW_MY_PACKS':
             return {...state, user_id: action.payload.user_id}
         default:
             return state
@@ -86,67 +87,65 @@ export const PacksReducer = (state: InitialStateType = initialState, action: Act
 }
 //=============================AC======================================
 export const setPacksAC = (resObj: ResponseCardsType) => ({
-    type: "PACKS/SET_PACKS",
+    type: 'PACKS/SET_PACKS',
     payload: resObj
 
 } as const)
 
 export const changePageAC = (page: number) => ({
-    type: "PACKS/CHANGE_PAGE",
+    type: 'PACKS/CHANGE_PAGE',
     payload: {page}
 } as const)
 
 export const changeMinAC = (min: number) => ({
-    type: "PACKS/CHANGE_MIN",
+    type: 'PACKS/CHANGE_MIN',
     payload: {min}
 } as const)
 
 export const changeMaxAC = (max: number) => ({
-    type: "PACKS/CHANGE_MAX",
+    type: 'PACKS/CHANGE_MAX',
     payload: {max}
 } as const)
 
 export const changePageCountAC = (pageCount: number) => ({
-    type: "PACKS/CHANGE_PAGE_COUNT",
+    type: 'PACKS/CHANGE_PAGE_COUNT',
     payload: {pageCount}
 } as const)
 
 export const changeSortPacksAC = (sortPacks: string) => ({
-    type: "PACKS/CHANGE_SORT_PACK",
+    type: 'PACKS/CHANGE_SORT_PACK',
     payload: {sortPacks}
 } as const)
 
 export const sortPacksNameAC = (packName: string) => ({
-    type: "PACKS/SORT_PACKS_NAME",
+    type: 'PACKS/SORT_PACKS_NAME',
     payload: {packName}
 } as const)
 
 export const changeShowMyPacksAC = (user_id: string) => ({
-    type: "PACKS/CHANGE_SHOW_MY_PACKS",
+    type: 'PACKS/CHANGE_SHOW_MY_PACKS',
     payload: {user_id}
 } as const)
 
 
 //==============================TC============================
 
-export const SetCardsPackTC = (): AppThunk =>
+export const SetCardsPackTC = (packsSearch?: string): AppThunk =>
     async (dispatch, getState) => {
-
         dispatch(setStatusApp('loading'))
         try {
             let {page, min, max, pageCount, sortPacks, packName, user_id} = getState().Packs
             if (page === 1) page = null
             if (min === 0) min = null
             if (max === 0 || max === 110) max = null
-            if (sortPacks === "") sortPacks = null
-            if (packName === "") packName = null
-            if (user_id === "") user_id = null
-            if (user_id === "") user_id = null
-
+            if (sortPacks === '') sortPacks = null
+            if (packName === '') packName = null
+            if (!!packsSearch) packName = packsSearch
+            if (user_id === '') user_id = null
+            if (user_id === '') user_id = null
             const res = await packsAPI.getPacks({min, max, page, pageCount, sortPacks, packName, user_id})
-
-               dispatch(setPacksAC(res.data))
-               dispatch(setStatusApp('succeeded'))
+            dispatch(setPacksAC(res.data))
+            dispatch(setStatusApp('succeeded'))
         } catch
             (e) {
             const err = e as Error | AxiosError
@@ -174,6 +173,7 @@ export const UpdatePackTC = (cardsPack: RequestUpdatePackType): AppThunk => asyn
     try {
         await packsAPI.updatePack(cardsPack)
         await dispatch(SetCardsPackTC())
+        dispatch(setPackNameForCardAC(cardsPack.name))
         dispatch(setStatusApp('succeeded'))
     } catch (e) {
         const err = e as Error | AxiosError
@@ -189,6 +189,7 @@ export const DeletePackTC = (idPack: string): AppThunk => async (dispatch) => {
     try {
         await packsAPI.deletePack(idPack)
         await dispatch(SetCardsPackTC())
+        dispatch(setPacksIdAC(''))
         dispatch(setStatusApp('succeeded'))
     } catch (e) {
         const err = e as Error | AxiosError
